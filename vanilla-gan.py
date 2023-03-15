2914108af7f5229724bbde96121a2cf1a43d0d4a
Saiful Islam <islam.saiful03@outlook.com>
2023-03-15 11:28:57 +0600

    discriminator included

--- vanilla-gan.py
+++ vanilla-gan.py
@@ -18,7 +18,6 @@ os.makedirs('output', exist_ok=True)
 
 img_shape = (1, 28, 28)
 
-
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
@@ -36,3 +35,21 @@ class Generator(nn.Module):
         x = F.leaky_relu(self.in3(self.fc3(x)),0.2)
         x = F.tanh(self.fc4(x))
         return x.view(x.shape[0],*img_shape)
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.fc1 = nn.Linear(28*28, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128,1)
+
+    def forward(self, x):
+        x = x.view(x.size(0),-1)
+        x = F.leaky_relu( self.fc1(x),0.2)
+        x = F.leaky_relu(self.fc2(x),0.2)
+        x = F.leaky_relu(self.fc3(x),0.2)
+        x = F.sigmoid(self.fc4(x))
+        return x
